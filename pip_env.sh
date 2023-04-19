93062602de0b4a4f2676915d334e579a436b07c1
Aditya Sirvaiya <aditya.sirvaiya@intel.com>
2023-04-19 09:20:01 +0530

    Update pip_env.sh

--- pip_env.sh
+++ pip_env.sh
@@ -9,5 +9,5 @@ pip install --upgrade pip
 #pip install tensorflow neural-compressor runipy notebook ipykernel  tensorflow_hub tensorflow_datasets
 pip install torch==1.13.1+cpu torchvision==0.14.1+cpu -f https://download.pytorch.org/whl/torch_stable.html  ## install torch cpu + torchvision
 pip install intel_extension_for_pytorch==1.13.100 -f https://developer.intel.com/ipex-whl-stable-cpu #install IPEX
-pip install transformers matplotlib
+pip install transformers matplotlib ipykernel
 python -m ipykernel install --user --name $ENV_NAME  #Register the env as a kernal for using it with jupyter notebook
